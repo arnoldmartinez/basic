c6b1092f28505f2593392844df9898176dcc630b
Arnold Martinez <arnoldmartinez@criptext.com>
2022-01-04 22:53:18 -0600

    Laravel 8 authentication install Cap. 18

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/contact', [ContactController::class, 'index'])->name('con');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
