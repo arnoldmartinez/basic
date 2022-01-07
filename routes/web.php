cbd9d301f9c19fff3eb5020e717a5e04ece7ee42
Arnold Martinez <arnoldmartinez@criptext.com>
2022-01-07 12:15:53 -0600

    Query builder read users data Cap. 21

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ContactController;
-use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,7 +15,8 @@ Route::get('/about', function () {
 Route::get('/contact', [ContactController::class, 'index'])->name('con');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    $users = User::all();
+    // $users = User::all();
+    $users = DB::table('users')->get();
 
     return view('dashboard', compact('users'));
 })->name('dashboard');
