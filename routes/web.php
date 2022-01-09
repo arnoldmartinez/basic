18614fee870a327460196f47648784cc9b816e8b
Arnold Martinez <arnoldmartinez@criptext.com>
2022-01-09 13:26:57 -0600

    Create model and migration Cap. 22

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
@@ -13,6 +14,10 @@ Route::get('/about', function () {
 });
 
 Route::get('/contact', [ContactController::class, 'index'])->name('con');
+
+// Category Controller
+Route::get('/category/all', [CategoryController::class, 'AllCat'])->name('all.category');
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     // $users = User::all();
