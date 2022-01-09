0a8ecd2fb5e21a19dee3cb707c802dbff7f7cfa6
Arnold Martinez <arnoldmartinez@criptext.com>
2022-01-09 14:43:27 -0600

    Form validation & show custom error message Cap. 23

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('/contact', [ContactController::class, 'index'])->name('con');
 // Category Controller
 Route::get('/category/all', [CategoryController::class, 'AllCat'])->name('all.category');
 
+Route::post('/category/add', [CategoryController::class, 'AddCat'])->name('store.category');
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     // $users = User::all();
