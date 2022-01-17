5e8c5aa630d6ad5164bccf8b1504dda969e974d4
Arnold Martinez <arnoldmartinez@criptext.com>
2022-01-17 08:51:46 -0600

    Laravel pagination Cap. 28

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,8 +13,8 @@ class CategoryController extends Controller
 {
     public function AllCat()
     {
-        //$categories = Category::latest()->get();
-        $categories = DB::table('categories')->latest()->get();
+        $categories = Category::latest()->paginate(5);
+        //$categories = DB::table('categories')->latest()->paginate(5);
 
         return view('admin.category.index', compact('categories'));
     }
